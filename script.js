2731fff0352410293af88db7064b849672184bda
Casmir Onyekani <casmir_ao@ymail.com>
2023-06-17 04:09:19 +0100

    added comments

--- script.js
+++ script.js
@@ -14,35 +14,51 @@ function openTab(tabName) {
   document.getElementById(tabName).classList.add("active-tab");
 }
 
+//Add event listener to the document object for the DOMContentLoaded event
 document.addEventListener("DOMContentLoaded", function () {
-  var typeText = document.querySelector(".typeText");
-  var textToBeTyped = "Frontend developer and Technical Writer";
+  // Selects element with class typeDel
+  var typeDel = document.querySelector(".typeDel");
+  // Text to be animated
+  var textToAnimate = "Frontend developer and Technical Writer";
+  // Keeps track of the current index of the text
   var index = 0,
+    // Indicates whether the text is being added or deleted
     isAdding = true;
 
-  function playAnim() {
+  function Animate() {
     setTimeout(function () {
-      typeText.innerText = textToBeTyped.slice(0, index);
+      // Sets element text based on current index.
+      typeDel.innerText = textToAnimate.slice(0, index);
       if (isAdding) {
-        if (index > textToBeTyped.length) {
+        // Checks if the index exceeds the length of the text
+        if (index > textToAnimate.length) {
+          // Updates the flag to indicate that text is being deleted
           isAdding = false;
-
           setTimeout(function () {
-            playAnim();
+            // Calls the Animate function after a delay of 2 seconds
+            Animate();
           }, 2000);
+          // Exits the current iteration of the function
           return;
         } else {
+          // Increments the index to move to the next character
           index++;
         }
       } else {
+        // Checks if the index has reached 0. All text has been deleted
         if (index === 0) {
+          // Updates the flag to indicate that text is being added
           isAdding = true;
         } else {
+          // Decrements the index to move back to the previous character
           index--;
         }
       }
-      playAnim();
+      // Calls the Animate function recursively
+      Animate();
+      // Sets the delay between each iteration of the function to 0.12 seconds
     }, 120);
   }
-  playAnim();
+  // Calls the Animate function to start the animation
+  Animate();
 });
